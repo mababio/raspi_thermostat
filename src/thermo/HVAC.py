ce9b8c3646fb1a866651e986a59bde0bb029726c
Michael Ababio <michaelkwasi@gmail.com>
2018-07-04 00:09:00 -0400

    chnage sensor temp implementation

--- src/thermo/HVAC.py
+++ src/thermo/HVAC.py
@@ -5,6 +5,9 @@ from threading import Thread
 from threading import Lock
 import Temp as Temp
 from config import config
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 '''
 author: Michael
@@ -13,10 +16,9 @@ website: ababio.me
 
 purpose: HVAC is a model of the real life HVAC. It is responsible for constantly checking if the furnace is needed or
 the ac is needed
-by comparing the desire temp and the actual temp 
+by comparing the desire temp and the actual temp
 
 '''
-
 
 class HVAC(object):
 
@@ -25,9 +27,17 @@ class HVAC(object):
     furnace = Furnace.Furnace()
 
     def __init__(self):
-        self._run = True
-        worker = Thread(target=self.task)
-        worker.start()
+        event_handler = Current_temp_handler(self.temp_compare)
+        observer = Observer()
+        observer.schedule(event_handler, path=config.sensor_path, recursive=False)
+        observer.start()
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
 
     def extract_temp(self, file_data):
         p = re.compile('[0-9]{2}')
@@ -44,30 +54,29 @@ class HVAC(object):
             self.get_sensor_temp()
         return int(sensor_temp)
 
-    def task(self):
-        lock = Lock()
-        while self._run:
-            lock.acquire()
-            if (Temp.get_temp() + HVAC.sensitivity) > self.get_sensor_temp():
-                HVAC.furnace.on()
-                HVAC.ac.off()
-            elif (Temp.get_temp() + HVAC.sensitivity) < self.get_sensor_temp():
-                HVAC.ac.on()
-                HVAC.furnace.off()
-            elif (Temp.get_temp() + HVAC.sensitivity) == self.get_sensor_temp():
-                HVAC.furnace.off()
-                HVAC.ac.off()
-            else:
-                print('forgot about this corner case')
-            lock.release()
+    #Turning on/off furnace or ac base on set and current temp
+    def temp_compare(self):
+        if (Temp.get_temp() + HVAC.sensitivity) > self.get_sensor_temp():
+            HVAC.furnace.on()
+            HVAC.ac.off()
+        elif (Temp.get_temp() + HVAC.sensitivity) < self.get_sensor_temp():
+            HVAC.ac.on()
+            HVAC.furnace.off()
+        elif (Temp.get_temp() + HVAC.sensitivity) == self.get_sensor_temp():
+            HVAC.furnace.off()
+            HVAC.ac.off()
+        else:
+            print('forgot about this corner case')
+
+class Current_temp_handler(FileSystemEventHandler):
+
+    def __init__(self, fun):
+        self.fun =  fun
+
+    def on_modified(self, event):
+        self.fun()
 
 
 if __name__ == "__main__":
 
     obj = HVAC()
-
-
-
-
-
-
