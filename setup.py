f3cc4bff3f4c2c6fc87c59703538857f5a6db626
Michael Ababio <michaelkwasi@gmail.com>
2018-07-04 12:44:43 -0400

    added redis module to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 version = '.1'
 
-install_requires = ['flask', 'schedule', 'pyyaml', 'singleton-decorator']
+install_requires = ['flask', 'schedule', 'pyyaml', 'singleton-decorator','redis']
 
 
 setup(name='thermo',
