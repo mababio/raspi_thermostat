b3a0a36b6edf62fce237a92064cf6afba4d73208
Michael Ababio <michaelkwasi@gmail.com>
2018-07-16 13:47:30 -0400

    simplied and modularized

---
+++ src/local/sensor_watcher.py
@@ -0,0 +1,36 @@
+'''
+author: Michael
+email: michaelkwasi@gmail.com
+website: ababio.me
+purpose: Whenever sensor temp changes push to remote redis DB
+'''
+
+import thermo_util
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+def run():
+    event_handler = Current_temp_handler(thermo_util.push_sensor_temp)
+    observer = Observer()
+    observer.schedule(event_handler, path="data/thermo/", recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+class Current_temp_handler(FileSystemEventHandler):
+
+    def __init__(self, funt):
+        self.job =  funt
+
+    def on_modified(self, event):
+        self.job()
+
+
+if __name__ == "__main__":
+    run()
